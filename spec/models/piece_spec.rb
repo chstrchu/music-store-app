bf902c71ca64260dbe839c627c1c9c05a0b96181
Chester Chu <chstrchu@gmail.com>
2024-12-23 19:27:41 -0800

    Add and fix rspec

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -6,8 +6,20 @@ RSpec.describe 'Piece', type: :model do
     let!(:bach_piece) { Piece.create!(name: "Bach comp", "composer": "Bach") }
 
     it 'scopes results by given composer' do
-      expect(Piece.with_composer("Bach").count).to eq(1)
-      expect(Piece.with_composer("Bach").last.composer).to eq("Bach")
+      result = Piece.by_composer("Bach")
+      expect(result.count).to eq(1)
+      expect(result.last.composer).to eq("Bach")
+    end
+  end
+
+  context 'form scope' do
+    let!(:vivaldi_sonata) { Piece.create!(name: "Vivaldi comp", form: "sonata", "composer": "Vivaldi") }
+    let!(:vivaldi_concerto) { Piece.create!(name: "Vivaldi comp", form: "concerto", "composer": "Vivaldi") }
+
+    it 'scopes results by given form' do
+      result = Piece.by_form("sonata")
+      expect(result.count).to eq(1)
+      expect(result.first.form).to eq("sonata")
     end
   end
 end
